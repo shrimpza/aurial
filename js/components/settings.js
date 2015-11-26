1e7a2d5beda1f38e109451ce07aed8c44c558738
Kenneth Watson <git@shrimpworks.za.net>
2015-11-26 19:35:43 +0200

    make settings form work in a more React-y way, add option to use the subsonic demo server

--- js/components/settings.js
+++ js/components/settings.js
@@ -1,10 +1,18 @@
 var Settings = React.createClass({
+	getInitialState: function() {
+		return {
+			url: this.props.subsonic.url,
+			user: this.props.subsonic.user,
+			password: this.props.subsonic.password
+		};
+	},
+
 	save: function(e) {
 		e.preventDefault();
 
-		localStorage.setItem('url', $("#subsonic-settings .url").val());
-		localStorage.setItem('username', $("#subsonic-settings .username").val());
-		localStorage.setItem('password', $("#subsonic-settings .password").val());
+		localStorage.setItem('url', this.state.url);
+		localStorage.setItem('username', this.state.user);
+		localStorage.setItem('password', this.state.password);
 
 		var subsonic = new Subsonic(localStorage.getItem('url'), 
 									localStorage.getItem('username'), 
@@ -20,25 +28,51 @@ var Settings = React.createClass({
 		}
 	},
 
+	demo: function(e) {
+		e.preventDefault();
+		if (confirm("Reconfigure to use the Subsonic demo server?")) {
+			this.setState({
+				url: "http://demo.subsonic.org",
+				user: "guest",
+				password: "guest"
+			});
+		}
+	},
+
+	change: function(e) {
+		switch (e.target.name) {
+			case "url": 
+				this.setState({url: e.target.value});
+				break;
+			case "user": 
+				this.setState({user: e.target.value});
+				break;
+			case "password": 
+				this.setState({password: e.target.value});
+				break;
+		}
+	},
+
 	render: function() {
 		return (
 			<div className="ui basic segment" id="subsonic-settings">
 				<form className="ui form" onSubmit={this.save}>
 					<div className="field">
 						<label>Subsonic URL</label>
-						<input className="url" placeholder="http://yourname.subsonic.com" type="text" defaultValue={this.props.subsonic.url} />
+						<input name="url" placeholder="http://yourname.subsonic.com" type="text" onChange={this.change} value={this.state.url} />
 					</div>
 					<div className="two fields">
 						<div className="field">
 							<label>Username</label>
-							<input className="username" placeholder="username" type="text" defaultValue={this.props.subsonic.user} />
+							<input name="user" placeholder="username" type="text" onChange={this.change} value={this.state.user} />
 						</div>
 						<div className="field">
 							<label>Password</label>
-							<input className="password" placeholder="password" type="text" defaultValue={this.props.subsonic.password} />
+							<input name="password" placeholder="password" type="password" onChange={this.change} value={this.state.password} />
 						</div>
 					</div>
-					<button className="ui button" type="submit">Save</button>
+					<button className="ui green button" type="submit">Save</button>
+					<button className="ui button" onClick={this.demo}>Demo</button>
 				</form>
 			</div>
 		);
