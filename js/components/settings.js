9d11f3df8173b3485d8f19c82973980fbfd37103
Kenneth Watson <git@shrimpworks.za.net>
2016-01-25 20:09:07 +0200

    add some preferences

--- js/components/settings.js
+++ js/components/settings.js
@@ -10,6 +10,8 @@ var Settings = React.createClass({
 			url: this.props.subsonic.url,
 			user: this.props.subsonic.user,
 			password: this.props.subsonic.password,
+			notifications: localStorage.getItem('notifications') === 'true',
+			backgroundArt: localStorage.getItem('backgroundArt') === 'true',
 			testState: TEST_UNTESTED
 		};
 	},
@@ -20,6 +22,9 @@ var Settings = React.createClass({
 		localStorage.setItem('url', this.state.url);
 		localStorage.setItem('username', this.state.user);
 		localStorage.setItem('password', this.state.password);
+
+		localStorage.setItem('notifications', this.state.notifications);
+		localStorage.setItem('backgroundArt', this.state.backgroundArt);
 
 		var subsonic = new Subsonic(localStorage.getItem('url'), 
 									localStorage.getItem('username'), 
@@ -87,7 +92,15 @@ var Settings = React.createClass({
 			case "password": 
 				this.setState({password: e.target.value});
 				break;
+			case "notifications": 
+				this.setState({notifications: e.target.checked});
+				break;
+			case "backgroundArt": 
+				this.setState({backgroundArt: e.target.checked});
+				break;
 		}
+
+		console.log(this.state);
 
 		this.setState({testState: TEST_UNTESTED});
 	},
@@ -111,6 +124,9 @@ var Settings = React.createClass({
 		return (
 			<div className="ui basic segment" id="subsonic-settings">
 				<form className="ui form" onSubmit={this.save}>
+					<h3 className="ui dividing header">
+	  					Subsonic Connection
+					</h3>
 					<div className="field">
 						<label>Subsonic URL</label>
 						<input name="url" placeholder="http://yourname.subsonic.com" type="text" onChange={this.change} value={this.state.url} />
@@ -125,8 +141,27 @@ var Settings = React.createClass({
 							<input name="password" placeholder="password" type="password" onChange={this.change} value={this.state.password} />
 						</div>
 					</div>
+
+					<h3 className="ui dividing header">
+	  					Preferences
+					</h3>
+					<div className="field">
+						<div className="ui checkbox">
+							<input name="notifications" type="checkbox" onChange={this.change} checked={this.state.notifications}/>
+							<label>Enable desktop notifications</label>
+						</div>
+					</div>
+					<div className="field">
+						<div className="ui checkbox">
+							<input name="backgroundArt" type="checkbox" onChange={this.change} checked={this.state.backgroundArt}/>
+							<label>Enable background art</label>
+						</div>
+					</div>
+
+					<div className="ui section divider"></div>
+
 					<button className="ui blue button" type="submit">Save</button>
-					<button className="ui button" onClick={this.demo}>Demo</button>
+					<button className="ui button" onClick={this.demo}>Demo Server</button>
 					<button className="ui icon button" onClick={this.test}>
 						<i className={testIcon + " icon"}></i>
 						Test Connection
