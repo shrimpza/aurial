1e06c9db42ae03f5168ef301466b6be95bb02b87
Kenneth Watson <git@shrimpworks.za.net>
2017-09-03 19:41:28 +0200

    fix prod build

--- src/webpack.dist.config.js
+++ src/webpack.dist.config.js
@@ -11,6 +11,11 @@ module.exports = {
     filename: 'index.js'
   },
   plugins: [
+		new webpack.DefinePlugin({
+		  'process.env': {
+		    NODE_ENV: JSON.stringify('production')
+		  }
+		}),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
